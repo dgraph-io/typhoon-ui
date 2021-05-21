2e1576d113cb3e83a8134203ead49f23403ac289
Mohit  Bajoria <mohitbajo36@gmail.com>
2021-05-21 12:53:06 +0530

    Fix

--- src/components/atoms/Avatar/Avatar.tsx
+++ src/components/atoms/Avatar/Avatar.tsx
@@ -1,66 +1,66 @@
 /** @jsx jsx */
-import { jsx } from "@emotion/core";
-import { styles } from "./Avatar.styles";
+import { jsx } from '@emotion/core';
+import { styles } from './Avatar.styles';
 
-import { stylegun } from "helpers/stylegun";
-import { useTheme } from "hooks/useTheme";
-import { AvatarPropsType } from "./Avatar.types.js";
+import { stylegun } from 'helpers/stylegun';
+import { useTheme } from 'hooks/useTheme';
+import { AvatarPropsType } from './Avatar.types';
 
-import { Stack, Text } from "components";
+import { Stack, Text } from 'components';
 
 const Avatar = ({
-    variant = 4,
-    round = true,
-    text,
-    bgColor = "black",
-    src = "https://svgshare.com/i/QA1.svg",
-    color = "white",
-    styles: stylesProp = {},
+  variant = 4,
+  round = true,
+  text,
+  bgColor = 'black',
+  src = 'https://svgshare.com/i/QA1.svg',
+  color = 'white',
+  styles: stylesProp = {},
 }: AvatarPropsType): JSX.Element => {
-    const theme = useTheme();
-    return (
-        <Stack
-            css={stylegun({
-                css: {
-                    ...styles.Avatar,
-                    ...(round ? styles.Round : {}),
-                    backgroundColor: text ? bgColor : {},
-                    ...stylesProp,
-                },
-                theme,
-                component: "Avatar",
-                variant,
-            })}
+  const theme = useTheme();
+  return (
+    <Stack
+      css={stylegun({
+        css: {
+          ...styles.Avatar,
+          ...(round ? styles.Round : {}),
+          backgroundColor: text ? bgColor : {},
+          ...stylesProp,
+        },
+        theme,
+        component: 'Avatar',
+        variant,
+      })}
+    >
+      {text ? (
+        <Text
+          variant={5}
+          weight={600}
+          styles={stylegun({
+            css: {
+              ...styles.Text,
+              color,
+            },
+            theme,
+          })}
         >
-            {text ? (
-                <Text
-                    variant={5}
-                    weight={600}
-                    styles={stylegun({
-                        css: {
-                            ...styles.Text,
-                            color,
-                        },
-                        theme,
-                    })}
-                >
-                    {text}
-                </Text>
-            ) : (
-                <img
-                    src={src}
-                    alt="avatar"
-                    css={stylegun({
-                        css: {
-                            ...styles.Image,
-                            ...(round ? styles.Round : {}),
-                        },
-                        theme,
-                    })}
-                />
-            )}
-        </Stack>
-    );
+          {text}
+        </Text>
+      ) : (
+        <img
+          src={src}
+          alt="avatar"
+          css={stylegun({
+            css: {
+              ...styles.Image,
+              ...(round ? styles.Round : {}),
+            },
+            theme,
+          })}
+        />
+      )}
+    </Stack>
+  );
 };
 
 export default Avatar;
