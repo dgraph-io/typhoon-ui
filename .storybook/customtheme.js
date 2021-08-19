b5f0b276ddcb7e1dea247f652e203c7492feb21f
tstreamDOTh <tstream.h@gmail.com>
2021-08-19 16:03:41 +0530

    chores: add link to gh repo

--- .storybook/customtheme.js
+++ .storybook/customtheme.js
@@ -4,5 +4,5 @@ export default create({
     base: "light",
     brandTitle: "typhoon-ui",
     brandUrl: "https://typhoon-ui.dgraph.io",
-    brandImage: `https://user-images.githubusercontent.com/11137394/130053143-498c0d26-0111-479c-a99c-31b82968ac1d.png`,
+    brandImage: `https://github.com/dgraph-io/typhoon-ui`,
 });
