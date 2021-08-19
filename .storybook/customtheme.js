eab5079c169d210bbc8c3dec198993fc9c93ecd4
tstreamDOTh <tstream.h@gmail.com>
2021-08-19 15:59:43 +0530

    chores:storybook logo update

---
+++ .storybook/customtheme.js
@@ -0,0 +1,8 @@
+import { create } from "@storybook/theming";
+
+export default create({
+    base: "light",
+    brandTitle: "typhoon-ui",
+    brandUrl: "https://typhoon-ui.dgraph.io",
+    brandImage: `https://user-images.githubusercontent.com/11137394/130053143-498c0d26-0111-479c-a99c-31b82968ac1d.png`,
+});
